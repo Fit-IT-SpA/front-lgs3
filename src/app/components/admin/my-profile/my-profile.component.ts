9e84eb4038e9722ed13e6f88c69afce4a1b61860
Nilemo <nicolas.leiva.moya@gmail.com>
2022-10-25 20:37:27 -0300

    Creacion de la Vista mi perfil
    se genero una la vista mi perfil, esta esta disenada en formato de acordeon con 2 botones, mi cuenta y negocios.
    en la pestana mi cuenta se muestra la info del dueno de los negcios
    en la pestana de mis negocios se muestran todos los negocios que pertenezcan al dueno, con su informacion correspondiente.
    
    Se hizo una evaluacion para saber si era posible hacer una reutilizacion de la api sin mayores cambios
    esta evaluacion concluyo con que era posible, por lo que la vista es completamente funcional
    
    #1203175012626240
    #1203173070558227

--- src/app/components/admin/my-profile/my-profile.component.ts
+++ src/app/components/admin/my-profile/my-profile.component.ts
@@ -4,6 +4,7 @@ import {ServiceTypeService} from '../../../shared/services/service-type.service'
 import {UserService} from '../../../shared/services/user.service';
 import { AuthService } from '../../../shared/services/firebase/auth.service';
 import { Router } from '@angular/router';
+import { Company } from '../../../shared/model/company';
 
 
 @Component({
@@ -17,14 +18,18 @@ export class MyProfileComponent implements OnInit{
     user: any;
     public perfil =  JSON.parse(localStorage.getItem('profile'));
     constructor(public authService: AuthService, private _router: Router, private userSrv: UserService) { }
+
     ngOnInit() {
-        console.log(this.perfil)
-        this.userSrv.findByRut(this.perfil.rut).subscribe(
+        //console.log(this.perfil)
+        this.userSrv.findByEmail(this.perfil.email).subscribe(
             (response) => {
                 this.user = response[0];
                 console.log(this.user);
-                console.log(this.perfil);
             });
+        console.log('alo');
+    //    console.log(this.companias.length);
+
+
     }
     logout(){
         localStorage.removeItem('profile');
