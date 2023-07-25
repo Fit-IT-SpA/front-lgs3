eca99ea846fc1d49ddc10b88086e4766b420d41a
belverino <nikosk8r@gmail.com>
2023-07-25 10:18:20 -0400

    se vuelve a dejar Offer en product como dato opcional necesario para desplegar mas rápidamente las offers en vista Product de Comercio

--- src/app/components/admin/orders/offers/offers-add/offers-add.component.ts
+++ src/app/components/admin/orders/offers/offers-add/offers-add.component.ts
@@ -116,7 +116,7 @@ export class OffersAddComponent implements OnInit {
                 this.offersFormGroup.controls.cantidad.setValue(0);
                 this.counter = 1;
                 this.modalService.dismissAll();
-                //this.product.offer = response.offer;
+                this.product.offer = response.offer;
             },
                 (error) => {
                     console.log(error);
