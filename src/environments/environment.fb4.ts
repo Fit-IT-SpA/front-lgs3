b49b37e616279be3e454b82e9a21e46ce6e0c667
Fit IT Admin <124718027+FitIT-Admin@users.noreply.github.com>
2023-08-23 11:15:21 -0400

    Create environment.fb4.ts

---
+++ src/environments/environment.fb4.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+        firebase: {
+        apiKey: "Your Api Key",
+        authDomain: "Your Auth Domain",
+        databaseURL: "Your Database Url",
+        projectId: "Your Project Id",
+        storageBucket: "Your StorageBucket url",
+        messagingSenderId: "Your Sender Id"
+      },
+    serverUrl: "https://tuercasfb4.fitit.dev/api"
+};
