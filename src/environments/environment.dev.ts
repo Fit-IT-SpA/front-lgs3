c4d368fad984fb17dd4b54e7640812fdc695e5e3
Fit IT Admin <124718027+FitIT-Admin@users.noreply.github.com>
2023-08-25 16:39:23 -0400

    Update environment.dev.ts

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -8,5 +8,5 @@ export const environment = {
         storageBucket: "Your StorageBucket url",
         messagingSenderId: "Your Sender Id"
       },
-    serverUrl: "https://subdev.fittedit.com/api"
+    serverUrl: "https://meson.planetatuercas.cl.com/api"
 };
